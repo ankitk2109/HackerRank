dd85035e2549f9f39b9b885577f4124c17592a5d
Ankit Kumar <ideal21ankit@gmail.com>
2019-11-11 15:45:01 +0000

    Arrays

---
+++ 30DaysOfCode/Day6/Arrays.py
@@ -0,0 +1,18 @@
+# Problem Statement: https://www.hackerrank.com/challenges/30-arrays/
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    print(" ".join(map(str,arr[::-1])))
