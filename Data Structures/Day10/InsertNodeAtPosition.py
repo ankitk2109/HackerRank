28593985546d8f48b0e17d55e5d90bad7c10da09
Ankit Kumar <ideal21ankit@gmail.com>
2019-11-19 23:05:10 +0000

    Inserting Node at Perticular Position

---
+++ Data Structures/Day10/InsertNodeAtPosition.py
@@ -0,0 +1,82 @@
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:   #If the Linked list is empty, point the head pointer to the first newly created node.
+            self.head = node
+        else:   #If the linked list has few elements then point the tail pointer of current node to new generated node
+            self.tail.next = node
+
+
+        self.tail = node #Point tail to the new added node i.e at the end.
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtPosition function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtPosition(head, data, position):
+    tail = head
+    node = SinglyLinkedListNode(data) #Creating Node
+    n = position-1 
+    while(n):
+        tail = tail.next #Going one position before the actual inserting position
+        n -= 1
+    node.next = tail.next
+    tail.next = node
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head, ' ', fptr)
+    fptr.write('\n')
+
+    fptr.close()
