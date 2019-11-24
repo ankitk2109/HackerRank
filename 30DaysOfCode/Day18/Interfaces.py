af1fc14cbf3f114f7875c814c2b05ac1d812637a
Ankit Kumar <ideal21ankit@gmail.com>
2019-11-24 12:45:52 +0000

    Calculator using Interfaces

---
+++ 30DaysOfCode/Day18/Interfaces.py
@@ -0,0 +1,19 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum_ = 0
+        for i in range(1,n+1):
+            if n%i==0:
+                sum_ = sum_+i
+        return sum_
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+#print(type(my_calculator).__bases__[0]) #__bases__ return a tuple of base classes wrt to current class. __name__ fetches the name of base class.   
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
