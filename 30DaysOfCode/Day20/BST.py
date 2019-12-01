fbd391ccf044de0b3b12985395376c9178e4da4e
Ankit Kumar <ideal21ankit@gmail.com>
2019-12-01 14:11:31 +0000

    Added function to get height

--- 30DaysOfCode/Day20/BST.py
+++ 30DaysOfCode/Day20/BST.py
@@ -1,3 +1,5 @@
+#Problem Statement at: https://www.hackerrank.com/challenges/30-binary-search-trees/
+
 class Node:
     def __init__(self,data):
         self.right=self.left=None
@@ -16,7 +18,9 @@ class Solution:
         return root
 
     def getHeight(self,root):
-        #Write your code here
+        if root == None:
+            return -1 
+        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
 
 T=int(input())
 myTree=Solution()
