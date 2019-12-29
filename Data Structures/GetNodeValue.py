f0517f158d00a357918705c535ec95a7f2c1de22
Ankit Kumar <ideal21ankit@gmail.com>
2019-12-29 01:37:10 +0000

    Get a Node Value from Last in Linked List

---
+++ Data Structures/GetNodeValue.py
@@ -0,0 +1,87 @@
+#Problem Statement at: https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the getNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def getNode(head, positionFromTail):
+    start = head
+    count = 1
+    if start == None:
+        return 
+    if start.next == None:
+        return head.data
+    while(start.next != None): #Calculating the number of nodes in the list
+        start = start.next
+        count += 1
+
+    newPos = count - positionFromTail - 1 # if we substract (positionFromLast+1), it will give us the position from start. 
+
+    for _ in range(newPos):
+        head = head.next
+    return head.data
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        position = int(input())
+
+        result = getNode(llist.head, position)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
